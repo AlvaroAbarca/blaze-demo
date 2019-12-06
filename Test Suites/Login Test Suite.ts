8fbec8fdcdc993edb84cf536795485e3977bf888
Alvaro Abarca <dead.time.aa@gmail.com>
2019-12-05 22:29:49 -0300

    Test Suites

---
+++ Test Suites/Login Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>dead.time@live.cl;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>57cf4132-e553-4a46-aa21-8b64605602a7</testSuiteGuid>
+   <testCaseLink>
+      <guid>c21604ae-9ade-4e23-886e-f93aae2e4516</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89b71127-ea74-4218-a3dc-215425703068</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
